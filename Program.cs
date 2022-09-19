00eb0187a400ae5eebe2237646ace771a7452fa9
Денис Шурыгин <disa-23ru@rambler.ru>
2022-09-19 21:48:35 +0500

    добавил максимальную сумму комиссии 60000 рупей

--- Program.cs
+++ Program.cs
@@ -11,7 +11,13 @@ namespace Project {
             double USD = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите актуальный кросс-курс пары EUR/USD на рынке ФОРЕКС: ");
             double Kross = Convert.ToDouble(Console.ReadLine());
-                double RUB = (USD * 1.00923 * SUMM + 60000)*Kross;
+                double RUB = USD * 1.00923 * SUMM * Kross;
+                if (RUB > 3000000) {
+                    RUB = RUB + 60000;
+                }
+                else {
+                    RUB = USD * 1.00923 * SUMM * Kross * 1.03;
+                }
             Console.WriteLine("Необходимая сумма в рублях при покупке USD по стоимости {0} с конвертацией в EUR (с учетом всех сраных комиссий этого гребанного Газпромбанка) = {1}", USD, (Math.Round(RUB, 2)));
         }
 
